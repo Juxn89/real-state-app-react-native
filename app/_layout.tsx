e4f7bc0a2393a917d032c4714bbecb68b5b74001
Juan Gómez <gb.jc@outlook.com>
2025-12-30 20:05:04 +0000

    Sign in screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,29 @@
-import { Stack } from 'expo-router';
+import { useEffect } from 'react'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
 import './global.css'
 
+SplashScreen.preventAutoHideAsync()
+
 export default function RootLayout() {
-  return <Stack />;
+	const [ fontsLoaded, error ] = useFonts({
+		'Rubik-Regular': require('../assets/fonts/Rubik-Regular.ttf'),
+		'Rubik-Bold': require('../assets/fonts/Rubik-Bold.ttf'),
+		'Rubik-ExtraBold': require('../assets/fonts/Rubik-ExtraBold.ttf'),
+		'Rubik-Medium': require('../assets/fonts/Rubik-Medium.ttf'),
+		'Rubik-SemiBold': require('../assets/fonts/Rubik-SemiBold.ttf'),
+		'Rubik-Light': require('../assets/fonts/Rubik-Light.ttf')
+	})
+
+	useEffect(() => {
+		if(fontsLoaded || error)
+			SplashScreen.hideAsync()
+	}, [fontsLoaded, error])
+
+	if(!fontsLoaded && !error)
+		return null
+
+  return <Stack 
+		screenOptions={{ headerShown: false }}
+	/>;
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,36 @@
-import { Text, View } from 'react-native'
+import icons from '@/constants/icons'
+import images from '@/constants/images'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export const SignIn = () => {
+	const handleLogin = () => {}
+
 	return (
-		<View>
-			<Text>sign-in</Text>
-		</View>
+		<SafeAreaView className='bg-white h-full'>
+			<ScrollView contentContainerClassName='h-full'>
+				<Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain'/>
+				<View className='px-10'>
+					<Text className='text-base text-center uppercase font-rubik text-black-200'>
+						Welcome to ReState
+					</Text>
+					<Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+						{ `Let's Get You Closer to \n` }
+						<Text className='text-primary-300'>Your Ideal Home</Text> 
+					</Text>
+					<Text className='text-lg font-rubik text-black-200 text-center mt-12'>Login to ReState with Google</Text>
+					<TouchableOpacity
+						className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'
+						onPress={ handleLogin }
+					>
+						<View className='flex flex-row items-center justify-center'>
+							<Image className='w-5 h-5' resizeMode='contain' source={icons.google} />
+							<Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continue with Google</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	)
 }
 
