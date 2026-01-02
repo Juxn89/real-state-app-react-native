ba2ea9c9fe0a6385adc32201e4bb1b317e84e91d
Juan Gómez <gb.jc@outlook.com>
2026-01-02 23:07:14 +0000

    Implemented home screen UI.

---
+++ components/filters/Filters.tsx
@@ -0,0 +1,53 @@
+import { categories } from "@/constants/data"
+import { router, useLocalSearchParams } from "expo-router"
+import { useState } from "react"
+import { ScrollView, Text, TouchableOpacity } from "react-native"
+
+export const Filters = () => {
+	const params = useLocalSearchParams<{filters?: string}>()
+	const [selectedCategory, setSelectedCategory] = useState(params.filters || 'All')
+	
+	const handleCategory = (category: string) => {
+		if(selectedCategory === category) {
+			setSelectedCategory('All')
+			router.setParams({ filter: 'All' })
+			return
+		}
+
+		setSelectedCategory(category)
+		router.setParams({ filter: category })
+	}
+
+	return (
+		<ScrollView 
+			horizontal
+			showsHorizontalScrollIndicator={ false }
+			className="mt-3 mb-2"
+		>
+			{
+				categories.map((item, index) => (
+					<TouchableOpacity 
+						key={index}
+						onPress={ () => handleCategory(item.category) }
+						className={ `flex flex-col items-start mr-4 px-4 py-2 rounded-full
+							${ selectedCategory === item.category 
+								? 'bg-primary-300' 
+								: 'border-t-primary-100 border border-primary-200' 
+						}` }
+					>
+						<Text
+							className={ 
+								`text-sx ${ 
+									selectedCategory === item.category 
+									? 'text-white font-rubik-bold mt-0.5' 
+									: 'text-black-300 font-rubik' }` 
+							}
+						>
+							{ item.title }
+						</Text>
+					</TouchableOpacity>
+				))
+			}
+		</ScrollView>
+	)
+}
--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,8 @@
+export { Search } from './search/Search'
 export { TabIcon } from './tab-icon/TabIcon'
 export { SettingsItem } from './settings-item/SettingsItem'
+
+export { Card } from './cards/Card'
+export { FeaturedCard } from './cards/FeaturedCard'
+
+export { Filters } from './filters/Filters'
